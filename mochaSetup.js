a329da64eb4ad00f3f0d73d2b564645147135658
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-10-19 14:21:40 +0300

    Setup mocha

---
+++ mochaSetup.js
@@ -0,0 +1,9 @@
+import JSDOM from "jsdom";
+
+const jsdom = new JSDOM("<body></body>", {
+  url: "https://example.org",
+});
+
+global.window = jsdom.window;
+global.document = jsdom.window.document;
+global.FormData = jsdom.window.FormData;
