7eba1fb8bd94a8f2c0269a1da39f2b6a14843e45
Ratmir Tsaregorodtsev <RTsaregorodtsev@alabuga.ru>
2023-10-20 16:20:12 +0300

    HTTP Client

--- src/shared/api/HTTPClient.spec.ts
+++ src/shared/api/HTTPClient.spec.ts
@@ -23,6 +23,31 @@ describe("HTTP transport", () => {
 
   it("should stringify query object for GET request where all parameters are strings", () => {
     http.get("", { data: { a: "1", b: "2" } });
+
     expect(request).calledWithMatch("?a=1&b=2", "GET");
+  });
+
+  it("should stringify query object for GET request where parameters are strings and numbers", () => {
+    http.get("", { data: { a: 1, b: "string" } });
+
+    expect(request).calledWithMatch("?a=1&b=string", "GET");
+  });
+
+  it("should encode characters for query", () => {
+    http.get("", { data: { a: "1+2", b: "2 2" } });
+
+    expect(request).calledWithMatch("?a=1%2B2&b=2%202", "GET");
+  });
+
+  it("should encode special characters for query", () => {
+    http.get("", { data: { a: "1=2&1" } });
+
+    expect(request).calledWithMatch("?a=1%3D2%261", "GET");
+  });
+
+  it("should encode special characters in parameter name for GET query", () => {
+    http.get("", { data: { "a=x&4": "q=w&e" } });
+
+    expect(request).calledWithMatch("?a%3Dx%264=q%3Dw%26e", "GET");
   });
 });
